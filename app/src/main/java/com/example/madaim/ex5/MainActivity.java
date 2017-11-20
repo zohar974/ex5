741632ede0111301dd7b7524487bd91258f23d73
zohar974 <zoharrotembar@gmail.com>
2017-11-20 16:52:58 +0200

    123123123

--- app/src/main/java/com/example/madaim/ex5/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex5/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
